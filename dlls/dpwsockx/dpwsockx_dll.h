d3976e54a33fab2f842d0d17ddb40513ff4215e1
Ismael Barros <razielmine@gmail.com>
2008-10-16 12:04:42 +0200

    dpwsockx: Implementation of SPInit

--- dlls/dpwsockx/dpwsockx_dll.h
+++ dlls/dpwsockx/dpwsockx_dll.h
@@ -21,8 +21,39 @@
 
 #include "windef.h"
 #include "winbase.h"
+#include "winsock2.h"
 #include "winnt.h"
 #include "wine/dplaysp.h"
+
+
+typedef struct tagDPWS_THREADDATA
+{
+    BOOL           is_running;
+    SOCKET         sock;
+    SOCKADDR_IN    addr;
+    HANDLE         handle;
+    LPDIRECTPLAYSP lpISP;
+} DPWS_THREADDATA, *LPDPWS_THREADDATA;
+
+typedef struct tagDPWS_DATA
+{
+    SOCKET          sock;
+    DPWS_THREADDATA tcp_listener, udp_listener, dplaysrv;
+    SOCKADDR_IN     nameserverAddr;
+    LPDIRECTPLAYSP  lpISP;
+} DPWS_DATA, *LPDPWS_DATA;
+
+typedef struct tagDPSP_MSG_HEADER
+{
+    DWORD       size;           /* size & 0x000FFFFF, token & 0xFFF00000 */
+    SOCKADDR_IN SockAddr;
+} DPSP_MSG_HEADER, *LPDPSP_MSG_HEADER;
+typedef const DPSP_MSG_HEADER* LPCDPSP_MSG_HEADER;
+
+/* DPSP_MSG_HEADER->token */
+#define DPSP_MSG_TOKEN_REMOTE    0xFAB00000
+#define DPSP_MSG_TOKEN_FORWARDED 0xCAB00000
+#define DPSP_MSG_TOKEN_SERVER    0xBAB00000
 
 
 HRESULT WINAPI SPInit( LPSPINITDATA );
